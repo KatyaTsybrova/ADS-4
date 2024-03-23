6f55edb3228fe7ae89e5d2f14eecdad6e3051dc6
KatyaTsybrova <160347264+KatyaTsybrova@users.noreply.github.com>
2024-03-23 21:32:56 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -14,14 +14,14 @@ int countPairs1(int *arr, int len, int value) {
 int countPairs2(int *arr, int len, int value) {
   int count = 0;
   int right = len - 1;
-  while ( arr[right] > value ) {
+  while (arr[right] > value) {
     --right;
   }
   int i = 0;
   int g = 1;
-  while ( i <= right ) {
-    while ( g <= right ) {
-      if ( arr[i] + arr[g] == value ) {
+  while (i <= right) {
+    while (g <= right) {
+      if (arr[i] + arr[g] == value) {
         count++;
       }
       g++;
@@ -33,41 +33,52 @@ int countPairs2(int *arr, int len, int value) {
 }
 
 int cbinsearch(int *arr, int size, int value) {
-  int count = 0;
-  int left = 0;
-  int right = size - 1;
-  while (left <= right) {
-    int mid = left + (right - left) / 2;
-    if (arr[mid] == value) {
-        count++;
-        int temp = mid - 1;
-        while (temp >= 0 && arr[temp] == value) {
-            count++;
-            temp--;
-        }
-        temp = mid + 1;
-        while (temp < size && arr[temp] == value) {
-            count++;
-            temp++;
-        }
-        break;
-    } else if (arr[mid] < value) {
-      left = mid + 1;
-    } else {
-      right = mid - 1;
-    }
+  int middle = left + (right - left) / 2;
+  if (right < left) {
+    return -42;
   }
-  return count;
+  if (arr[middle] == value) {
+    return middle;
+  }
+  if (arr[middle] > value) {
+    return cbinsearch(arr, left, middle - 1, value);
+  } else {
+    return cbinsearch(arr, middle + 1, right, value);
+  }
+}
+
+int findenteres(int *arr, int len, int value) {
+  int index = cbinsearch(arr, 0, len - 1, value);
+  if (index == -42) {
+    return 0;
+  }
+  int counter = 1, left = index - 1;
+  while (true) {
+        if (!(left >= 0 && arr[left] == value)) {
+          break;
+        }
+        left -= 1;
+        counter += 1;
+  }
+  int right = index + 1;
+  while (true) {
+        if (!(right < 0 && arr[right] == value)) {
+          break;
+        }
+        counter += 1;
+        right += 1;
+  }
+  return counter;
 }
 
 int countPairs3(int *arr, int len, int value) {
   int count = 0;
   int right = len - 1;
-  while ( arr[right] > value ) {
+  while (arr[right] > value) {
     --right;
   }
-  while ( right >= 1 ) {
-    count += cbinsearch(arr, right - 1, value - arr[right]);
+  while (right >= 1) {
+    count += findenteres(arr, right - 1, value - arr[right]);
     --right;
   }
   return count;
