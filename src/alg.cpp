599a5fca0072991539a58133d69a07179379c3cf
KatyaTsybrova <160347264+KatyaTsybrova@users.noreply.github.com>
2024-03-23 17:10:11 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -16,12 +16,12 @@ int countPairs2(int *arr, int len, int value) {
   int left = 0;
   int right = len - 1;
   while (left < right) {
-    if (arr[left] + arr[right] == value) {
+    int sum = arr[left] + arr[right];
+    if (sum == value) {
       count++;
       left++;
       right--;
-    }
-    else if (arr[left] + arr[right] < value) {
+    } else if (sum < value) {
       left++;
     } else {
       right--;
@@ -33,9 +33,11 @@ int countPairs2(int *arr, int len, int value) {
 int countPairs3(int *arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len; ++i) {
-    int index = binarySearch(arr, i + 1, len - 1, value - arr[i]);
-    if (index != -1) {
-      count++;
+    int complement = value - arr[i];
+    for (int j = i + 1; j < len; ++j) {
+      if (arr[j] == complement) {
+        count++;
+      }
     }
   }
   return count;
